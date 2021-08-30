a134aa92b84bc5cbaf70b50c1a09c5c9d322fd45
Eliza Plowden <elizaplowden@gmail.com>
2021-08-30 16:20:10 +0100

    associations and validatoins

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,5 @@
 class Movie < ApplicationRecord
   has_many :bookmarks
-  validates :title, uniqueness: :true
+  validates :title, uniqueness: :true, presence: true
+  validates :overview, presence: true
 end
