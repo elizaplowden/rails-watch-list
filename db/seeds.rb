5c6c8019e7c6f8ba3cdd18568d416ae39b295e1c
Eliza Plowden <elizaplowden@gmail.com>
2021-08-30 16:31:27 +0100

    added pseudocode to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,16 +13,23 @@ puts "Cleaning up database..."
 Movie.destroy_all
 puts "Database cleaned"
 
+# using Le Wagon proxy
 url = "http://tmdb.lewagon.com/movie/top_rated"
+# 10 movies
 10.times do |i|
   puts "Importing movies from page #{i + 1}"
+  # parse JSON, open the url and read it. then get results from object
   movies = JSON.parse(open("#{url}?page=#{i + 1}").read)['results']
+  # iterate over the movies
   movies.each do |movie|
     puts "Creating #{movie['title']}"
+    # svg image
     base_poster_url = "https://image.tmdb.org/t/p/original"
+    # create movie object using this info
     Movie.create(
       title: movie['title'],
       overview: movie['overview'],
+      # poster url with movie endpoint
       poster_url: "#{base_poster_url}#{movie['backdrop_path']}",
       rating: movie['vote_average']
     )
